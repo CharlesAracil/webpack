3e5a4bcef89192433e751ae8da4b9ad5972b81b5
Charles Aracil <charles.aracil@gmail.com>
2017-11-18 11:54:01 +0100

    connect django with webpack production files

--- template/frontend/config/index.js
+++ template/frontend/config/index.js
@@ -53,12 +53,12 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../../static/dist/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsRoot: path.resolve(__dirname, '../../static/dist'),
+    assetsSubDirectory: '',
+    assetsPublicPath: '/static/dist/',
 
     /**
      * Source Maps
